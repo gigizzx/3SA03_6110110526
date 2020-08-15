68bfd767ecd4187c7fd7eb12e9c4dc4cddf52dfd
gigizzx <6110110526@psu.ac.th>
2020-08-15 23:55:02 +0700

    part 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import WordCard from './WordCard';
 
+const word = "Hello" ;
 function App() {
   return (
     <div >
-    hello world 
+      <WordCard value={word}/>
     </div>
   );
 }
