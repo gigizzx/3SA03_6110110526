2dcd6065c846a32e199dbd7f324546c2248019cf
gigizzx <60774602+gigizzx@users.noreply.github.com>
2020-08-27 14:55:57 +0700

    Add files via upload
    part6

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import WordCard from './WordCard';
+
+const word = "Hello" ;
+function App() {
+  return (
+    <div >
+      <WordCard value={word}/>
+    </div>
+  );
+}
+
+export default App;
